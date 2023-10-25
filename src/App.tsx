2ff9d95701005579fe7b5d0c180ddceaa308060e
Andrew Smith <andrew.smith@moserit.com>
2023-10-25 16:08:20 -0400

    added a test custom prime theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './lib/router/router';
 import { PrimeReactProvider } from 'primereact/api';
-import 'primereact/resources/themes/lara-light-teal/theme.css';
+// import 'primereact/resources/themes/lara-light-teal/theme.css';
+import './styles/test-theme.css';
 
 function App() {
   return (
